707069a050f6f37cbd38c4295a3fdd84663504e1
javahometech <kammana.hari@gmail.com>
2022-07-06 08:09:50 +0530

    Create tomcatDeploy.groovy

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,8 @@
+def call(credId,userName,ipAddr){
+  sshagent([credId]) {
+    sh "mv target/*.war target/webapp.war"
+    sh "scp -o StrictHostKeyChecking=no target/webapp.war ${userName}@${ipAddr}:/opt/tomcat9/webapps/"
+    sh "ssh ${userName}@${ipAddr} /opt/tomcat9/bin/shutdown.sh"
+    sh "ssh ${userName}@${ipAddr} /opt/tomcat9/bin/startup.sh"
+  }
+}
